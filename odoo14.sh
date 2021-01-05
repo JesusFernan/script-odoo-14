5a8a4f1b04f63d66cd1c44041b096952a51158ed
Jesus Fernandez <fanthertec@gmail.com>
2021-01-05 14:43:25 -0700

    Ultimos cambios script

--- odoo14.sh
+++ odoo14.sh
@@ -9,3 +9,61 @@ wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtml
 wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.$(lsb_release -cs)_i386.deb"
 
 #Agregar repositorio universe y actualizar (Arregla error de descarga de librerias)
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install -y git
+
+# Actualizaz e instalar Postgresql
+sudo apt-get install postgresql -y
+sudo su - postgres -c "createuser -s $USER"
+
+mkdir ~/Developments
+mkdir $PATHBASE
+mkdir $PATHREPOS
+cd $PATHBASE
+# Download Odoo from git source
+git clone https://github.com/odoo/odoo.git -b $ODOOVERSION --depth $DEPTH
+
+# Instalar python3 and dependencias para Odoo
+sudo apt-get -y install gcc python3-dev libxml2-dev libxslt1-dev \
+ libevent-dev libsasl2-dev libldap2-dev libpq-dev \
+ libpng-dev libjpeg-dev
+
+sudo apt-get -y install python3 python3-pip python-pip
+sudo pip3 install libsass vobject qrcode num2words setuptools
+
+# Arreglar wkhtml* dependencias Ubuntu Server 18.04
+sudo apt-get -y install libxrender1
+
+# Instalar nodejs & less
+sudo apt-get install -y npm node-less
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g less
+
+
+# Descarga e instala  WKHTMLTOPDF
+rm $PATHBASE/wkhtmltox_0.12.5-1*.deb
+rm wkhtmltox_0.12.5-1*.deb
+if [ "`getconf LONG_BIT`" == "32" ];
+
+then
+	wget $wk32
+	wkhtmltox_0.12.5-1.bionic_amd64.deb
+else
+	wget $wk64
+fi
+
+sudo dpkg -i --force-depends wkhtmltox_0.12.5-1*.deb
+sudo ln -s /usr/local/bin/wkhtml* /usr/bin
+#tar xvf $PATHBASE/wkhtmltox*.tar.xz
+#sudo cp -r $PATHBASE/wkhtmltox/bin/* /usr/local/bin/
+#sudo cp -r $PATHBASE/wkhtmltox/bin/* /usr/bin/
+
+
+# instala dependencias de odoo
+sudo pip3 install -r $PATHBASE/odoo/requirements.txt
+sudo apt-get -f -y install
+python3 $PATHBASE/odoo/odoo-bin
+
+
+echo "Odoo 14 instalado correctamente: Jesús Fernández"
