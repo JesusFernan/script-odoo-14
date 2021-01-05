f0eb111f7262277e17d2a18880b0a52fb7481dd0
Jesus Fernandez <fanthertec@gmail.com>
2021-01-05 14:00:19 -0700

    primeros cambios script

---
+++ odoo14.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#GALARTEC CONSULTORES
+
+ODOOVERSION=14.0
+DEPTH=1
+
+#Libreria de Wkhtmltopdf para los distintas arquitecturas SO
+wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.$(lsb_release -cs)_amd64.deb"
+wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.$(lsb_release -cs)_i386.deb"
+
